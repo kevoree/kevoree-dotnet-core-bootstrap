2241c2b23d2e7f2be6e940ec4021e2a3c3a402ab
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-21 16:28:49 +0200

    wip

---
+++ NugetClassLoaderTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Org.Kevoree.Core.Bootstrap
+{
+	[TestFixture ()]
+	public class NugetClassLoaderTest
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			new NugetClassLoader ().Load ("org-kevoree-yield-version", "3.0.0");
+		}
+	}
+}
+
---
+++ SeparateAppDomainAssemblyLoaderTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using ConsoleApplication1;
+
+namespace Org.Kevoree.Core.Bootstrap
+{
+	[TestFixture ()]
+	public class SeparateAppDomainAssemblyLoaderTest
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+
+			Console.WriteLine (typeof(SeperateAppDomainAssemblyLoader).ToString());
+			var type = Type.GetType ("ConsoleApplication.SeperateAppDomainAssemblyLoader, SeperateAppDomainAssemblyLoader");
+			Activator.CreateInstance (type);
+			System.IO.FileInfo param = new System.IO.FileInfo ("/tmp/kevoreenuget/org-kevoree-yield-version.3.0.0/lib/net40/YieldVersion.dll");
+			var result = new SeperateAppDomainAssemblyLoader ().LoadAssembly(param);
+			Console.WriteLine (result);
+		}
+	}
+}
+
