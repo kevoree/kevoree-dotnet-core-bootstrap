def98fd3bafe08292ed911bf8b18f35d5fee5f2e
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-24 11:21:20 +0200

    Project type set to console application
    Plus Mono.Option integration.
    WIP.

--- NugetClassLoader.cs
+++ NugetClassLoader.cs
@@ -2,7 +2,6 @@
 using NuGet;
 using System.Reflection;
 using System.IO;
-using AppDomainToolkit;
 
 namespace Org.Kevoree.Core.Bootstrap
 {
@@ -16,7 +15,8 @@ namespace Org.Kevoree.Core.Bootstrap
 		{
 			
 			var repo = PackageRepositoryFactory.Default.CreateRepository ("https://packages.nuget.org/api/v2");
-			string path = "/tmp/kevoreenuget";
+            string finalPath = "Plugins";
+            string path = GetPath(packageName, version, finalPath);
 			var packageManager = new PackageManager (repo, path);
 
 			var AppDomainToolkitPackage = repo.FindPackage ("AppDomainToolkit", SemanticVersion.Parse ("1.0.4.3"));
@@ -28,34 +28,16 @@ namespace Org.Kevoree.Core.Bootstrap
 
 			if (package != null) {
 				packageManager.InstallPackage (package, false, true);
-				foreach (PhysicalPackageFile iPackageAssemblyReference in package.AssemblyReferences) {
-					//Assembly.LoadFile (iPackageAssemblyReference.SourcePath);
-					/*Type typeProxyType = typeof(TypeProxy);
-
-					//var type = typeProxyInstance.LoadFromAssembly (iPackageAssemblyReference.SourcePath, "Org.Kevoree.YieldVersion.YieldVersion");
-					object instance = domain.CreateInstanceAndUnwrap (iPackageAssemblyReference.SourcePath, "Org.Kevoree.YieldVersion.YieldVersion");
-					Console.WriteLine ("yes ?." + instance.ToString ());*/
-					var rootDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-					var setupInfo = new AppDomainSetup()
-					{
-						//ApplicationName = "My Application",
-						ApplicationBase = path,
-						PrivateBinPath = path 
-					};
-					using(var context = AppDomainContext.Create(setupInfo)) {
-						// chemin dans tmp : iPackageAssemblyReference.SourcePath
-						context.LoadAssemblyWithReferences (LoadMethod.LoadFile, path + "/" + packageName + "." + version + "/" + "lib/net40/YieldVersion.dll");
-						RemoteAction.Invoke (context.Domain, () => {
-							var msg = " from AppDomain " + AppDomain.CurrentDomain.CreateInstanceAndUnwrap("YieldVersion", "Org.Kevoree.YieldVersion.YieldVersion");
-							//var b = a.GetType("Org.Kevoree.YieldVersion.YieldVersion");
-							Console.WriteLine(msg);
-						});
-						Console.WriteLine ("okok");
-					}
-				}
 			}
 
 		}
+
+        public static string GetPath(string packageName, string version, string finalPath)
+        {
+            string rootPath = @"C:\Users\mleduc\Desktop\NUGET";
+            string path = Path.Combine(rootPath, packageName, version, finalPath);
+            return path;
+        }
 
 		/*private static void PackageManager_PackageInstalled(object sender, PackageOperationEventArgs e)
 		{
