872e7ee4122080e9101dbd0899337636bfc0eee1
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-28 12:07:20 +0200

    Arbitrary versino for backup sake

---
+++ Bootstrap.cs
@@ -0,0 +1,53 @@
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Kevoree.Core.Bootstrap
+{
+    class Bootstrap
+    {
+
+        static void Main(string[] args)
+        {
+            string nodeName = "node0";
+            string scriptPath = null;
+            bool showHelp = false;
+
+            var optionSet = new OptionSet()
+            {
+                { "node.name=", "the main node name (default: node0)", n => nodeName = n },
+                { "node.script=", "init script path", p => scriptPath = p },
+                { "h|help=", "displays help message", v => showHelp = true }
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = optionSet.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("kevoree-dotnet: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `kevoree-dotnet --help' for more information.");
+                return;
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(optionSet);
+                return;
+            }
+        }
+
+        private static void ShowHelp(OptionSet optionSet)
+        {
+            Console.WriteLine("Usage: kevoree-dotnet [OPTIONS]+");
+            Console.WriteLine("Options:");
+            optionSet.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
