e629a8146526e21dc8a2ea99a09cf9f74d73bb35
Manuel Leduc <manuel.leduc@inria.fr>
2015-10-06 15:14:40 +0200

    Compilable state, big part of the model is marshalled

--- NodeRunner.cs
+++ NodeRunner.cs
@@ -1,41 +1,57 @@
-﻿using org.kevoree;
-using org.kevoree.api;
-using Org.Kevoree.Annotation;
-using Org.Kevoree.Core.Api;
-using Org.Kevoree.Core.Api.Adaptation;
-using Org.Kevoree.Library.Annotation;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.ComponentModel.Composition.Registration;
 using System.Linq;
+using Org.Kevoree.Annotation;
+using Org.Kevoree.Core.Api;
+using Org.Kevoree.Core.Api.Adaptation;
+using Org.Kevoree.Core.Api.IMarshalled;
+using Org.Kevoree.Library.Annotation;
+using NodeType = Org.Kevoree.Core.Api.NodeType;
 
 namespace Org.Kevoree.Core.Bootstrap
 {
-
     public class NodeRunner : MarshalByRefObject, INodeRunner
     {
+        private readonly AnnotationHelper annotationHelpler = new AnnotationHelper();
         private CompositionContainer container;
         private DirectoryCatalog directoryCatalog;
-        Org.Kevoree.Annotation.DeployUnit node;
+        private readonly KevoreeInjector<KevoreeInject> injector = new KevoreeInjector<KevoreeInject>();
+        private DeployUnit node;
         private string pluginPath;
-        private readonly AnnotationHelper annotationHelpler = new AnnotationHelper();
-        private KevoreeInjector<KevoreeInject> injector = new KevoreeInjector<KevoreeInject>();
 
         public void setPluginPath(string pluginPath)
         {
             this.pluginPath = pluginPath;
         }
 
+        public AdaptationModel plan(IContainerRootMarshalled current, IContainerRootMarshalled target,
+            ITracesSequence sequence)
+        {
+            return ((NodeType) node).plan(current, target, sequence);
+        }
+
+        public PrimitiveCommand getPrimitive(AdaptationPrimitive primitive)
+        {
+            return ((NodeType) node).getPrimitive(primitive);
+        }
+
+        public void Start()
+        {
+            //this.Init();
+            var startMethod = annotationHelpler.filterMethodsByAttribute(node.GetType(), typeof (Start)).First();
+            startMethod.Invoke(node, null);
+        }
+
         private void Init()
         {
             // Use RegistrationBuilder to set up our MEF parts.
             var regBuilder = new RegistrationBuilder();
-            regBuilder.ForTypesDerivedFrom<Org.Kevoree.Annotation.DeployUnit>().Export<Org.Kevoree.Annotation.DeployUnit>();
+            regBuilder.ForTypesDerivedFrom<DeployUnit>().Export<DeployUnit>();
 
             var catalog = new AggregateCatalog();
-            catalog.Catalogs.Add(new AssemblyCatalog(typeof(NodeRunner).Assembly, regBuilder));
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof (NodeRunner).Assembly, regBuilder));
             directoryCatalog = new DirectoryCatalog(pluginPath, regBuilder);
             catalog.Catalogs.Add(directoryCatalog);
 
@@ -43,36 +59,16 @@ namespace Org.Kevoree.Core.Bootstrap
             container.ComposeExportedValue(container);
 
             // Get our exports available to the rest of Program.
-            var laliste = container.GetExportedValues<Org.Kevoree.Annotation.DeployUnit>();
+            var laliste = container.GetExportedValues<DeployUnit>();
             node = laliste.First();
-        }
-
-        public AdaptationModel plan(ContainerRoot actualModel, ContainerRoot targetModel)
-        {
-            // TODO
-            return null;
-        }
-
-        public Org.Kevoree.Core.Api.PrimitiveCommand getPrimitive(AdaptationPrimitive primitive)
-        {
-            // TODO
-            return null;
-        }
-
-        public void Start()
-        {
-            //this.Init();
-            var startMethod = this.annotationHelpler.filterMethodsByAttribute(node.GetType(), typeof(Start)).First();
-            startMethod.Invoke(node, null);
         }
 
         public void proceedInject(string path, string nodeName, string name, KevoreeCoreBean core)
         {
-            this.Init();
-            this.injector.inject<Context>(node, new InstanceContext(path, nodeName, name));
-            this.injector.inject<Org.Kevoree.Core.Api.ModelService>(node, new ContextAwareAdapter(core, path));
-            this.injector.inject<Org.Kevoree.Core.Api.BootstrapService>(node, core.getBootstrapService());
+            Init();
+            injector.inject<Context>(node, new InstanceContext(path, nodeName, name));
+            injector.inject<ModelService>(node, new ContextAwareAdapter(core, path));
+            injector.inject(node, core.getBootstrapService());
         }
     }
-
 }
