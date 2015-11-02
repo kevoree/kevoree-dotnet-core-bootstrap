78c294349e5052dc1ee1f3c7211525584ad4fea1
Manuel Leduc <manuel.leduc@inria.fr>
2015-11-02 20:37:07 +0100

    First release 1.0.0

---
+++ KevoreeKernel.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+
+namespace Org.Kevoree.Core.Microkernel
+{
+    public abstract class KevoreeKernel
+    {
+        public readonly static ThreadLocal<KevoreeKernel> Self = new ThreadLocal<KevoreeKernel>();
+    }
+}
